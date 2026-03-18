7bf763867eec4a7988000646960e95746f4eb349
Sunder Vasudevan <sunder.v@outlook.com>
2026-03-18 14:55:48 +0530

    ARIALogo: fix character spacing, remove inline-flex

--- src/components/ARIALogo.jsx
+++ src/components/ARIALogo.jsx
@@ -1,24 +1,23 @@
-// ARIA brand name: round dot on i in brand blue
 export default function ARIALogo({ className = '', style = {} }) {
   return (
-    <span className={className} style={{ display: 'inline-flex', alignItems: 'baseline', ...style }}>
-      {'AR'}
-      <span style={{ position: 'relative', display: 'inline-block', lineHeight: 'inherit' }}>
+    <span className={className} style={{ letterSpacing: 'inherit', ...style }}>
+      AR
+      <span style={{ position: 'relative', display: 'inline-block' }}>
         ı
         <span style={{
           position: 'absolute',
           left: '50%',
-          top: '0.05em',
+          top: '0.08em',
           transform: 'translateX(-50%)',
-          width: '0.2em',
-          height: '0.2em',
+          width: '0.18em',
+          height: '0.18em',
           borderRadius: '50%',
           background: '#1D6FDB',
           display: 'block',
           pointerEvents: 'none',
         }} />
       </span>
-      {'A'}
+      A
     </span>
   )
 }
